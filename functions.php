d661b396f9aae7bc5f49c99fe5b20caf5c8d5436
Cricfies <90220761+Cricfies@users.noreply.github.com>
2022-02-26 13:17:43 +0100

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,18 @@
+add_filter( 'woocommerce_product_single_add_to_cart_text', 'woocommerce_custom_single_add_to_cart_text' ); 
+function woocommerce_custom_single_add_to_cart_text() {
+    return __( 'Ajouter au PDF', 'woocommerce' ); 
+}
+
+// To change add to cart text on product archives(Collection) page
+add_filter( 'woocommerce_product_add_to_cart_text', 'woocommerce_custom_product_add_to_cart_text' );  
+function woocommerce_custom_product_add_to_cart_text() {
+    return __( 'Ajouter au PDF', 'woocommerce' );
+}
+
+add_filter( 'woocommerce_product_add_to_cart_text', function( $text ) {
+	global $product;
+	if ( $product->is_type( 'variable' ) ) {
+		$text = $product->is_purchasable() ? __( 'Ajouter au PDF', 'woocommerce' ) : __( 'Read more', 'woocommerce' );
+	}
+	return $text;
+}, 10 );
